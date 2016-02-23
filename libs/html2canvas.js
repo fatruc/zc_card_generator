f1c4e656667ebe7e5a322ca41976ee34c0ce829d
fatruc <luna*2877>
2016-02-23 17:19:47 +0100

    encore un essai

--- libs/html2canvas.js
+++ libs/html2canvas.js
@@ -1868,21 +1868,22 @@ function asFloat(str) {
     return parseFloat(str);
 }
 
-function getBounds(node) {
-    if (node.getBoundingClientRect) {
-        var clientRect = node.getBoundingClientRect();
-        var width = node.offsetWidth == null ? clientRect.width : node.offsetWidth;
-        return {
-            top: clientRect.top,
-            bottom: clientRect.bottom || (clientRect.top + clientRect.height),
-            right: clientRect.left + width,
-            left: clientRect.left,
-            width:  width,
-            height: node.offsetHeight == null ? clientRect.height : node.offsetHeight
-        };
+
+function getBounds (node) {
+        if (node.getBoundingClientRect) {
+            var clientRect = node.getBoundingClientRect();
+            var width = node.offsetWidth == null ? clientRect.width : node.offsetWidth;
+            return {
+                top   : Math.floor(clientRect.top),
+                bottom: Math.floor(clientRect.bottom || (clientRect.top + clientRect.height)),
+                right : Math.floor(clientRect.left + width),
+                left  : Math.floor(clientRect.left),
+                width : width,
+                height: node.offsetHeight == null ? clientRect.height : node.offsetHeight
+            };
+        }
+        return {};
     }
-    return {};
-}
 
 function offsetBounds(node) {
     var parent = node.offsetParent ? offsetBounds(node.offsetParent) : {top: 0, left: 0};
