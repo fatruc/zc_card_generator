0e3a88edf6f02a78d3978337e1683527392495f3
fatruc <luna*2877>
2016-03-04 17:13:08 +0100

    persistence de la carte et de son nom

--- src/cloud.js
+++ src/cloud.js
@@ -1,17 +1,21 @@
 
-var ref;
+
+var uid;
+
+var FIREBASE_APP_URL = "https://cartes-equip-zc.firebaseio.com/";
 
 $(document).ready(function() {
 	
-	ref = new Firebase("https://cartes-equip-zc.firebaseio.com/");
-	
+	var ref = new Firebase(FIREBASE_APP_URL);
 	ref.onAuth(authDataCallback);
 	
 	$("#google_connect").click(google_connect);
+	$("#upload_button").click(upload);
 
 });
 
 function google_connect(){
+	var ref = new Firebase(FIREBASE_APP_URL);
 	ref.authWithOAuthPopup("google", function(error, authData) {
 	  if (error) {
 		console.log("Login Failed!", error);
@@ -23,9 +27,46 @@ function google_connect(){
 
 // Create a callback which logs the current auth state
 function authDataCallback(authData) {
+  uid = authData.uid;
   if (authData) {
     console.log("User " + authData.uid + " is logged in with " + authData.provider);
   } else {
     console.log("User is logged out");
   }
+}
+
+function upload(){
+	var ref = new Firebase(FIREBASE_APP_URL + "users/" + uid + "/cards/");
+	var card_ref = ref.push()
+	card_ref.set(current_card, function(error) {
+		  if (error) {
+			alert("Data could not be saved." + error);
+		  } else {
+			  add_card_name(card_ref.key(), current_card);
+		  }
+	});
+}
+
+function add_card_name(card_id, card){
+	console.log(card_id);
+	
+	var ref = new Firebase(FIREBASE_APP_URL + "users/" + uid + "/card_names/");
+	
+	var card_name = new Object();
+	card_name.card_id=card_id;
+	
+	for(p in card){
+		if(p.startsWith("card_name_")){
+			card_name[p] = card[p];
+		}
+	}
+	
+	ref.push(card_name, function(error) {
+		  if (error) {
+			alert("Data could not be saved." + error);
+		  } else {
+			 alert("Data saved successfully.");
+		  }
+	});
+	
 }
